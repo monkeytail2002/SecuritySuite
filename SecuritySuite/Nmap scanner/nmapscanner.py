6d1da113b3fc365a3bc0875b051b75705c9de5c1
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-08-08 20:26:26 +0100

    Banner Grabber added
    Added a banner grabber for OpSec purposes.

--- SecuritySuite/Nmap scanner/nmapscanner.py
+++ SecuritySuite/Nmap scanner/nmapscanner.py
@@ -29,10 +29,28 @@ class Portscan(Scanning):
 				for port in scannedPorts:
 					print('port: %s\tstate: %s' % (port, nmScan[host][proto][port]['state']))
 
+#Class for grabbing banners (meta information about networks and servers for opsec
+class Bannergrab(Scanning):
+	def results(self):
+		#scan the ip range, port range and insert the banner script along with the -sV option
+		nmScan.scan(self.ipaddress, self.ports,arguments='-sV -script=banner')
+		#set the scan to return the host
+		hostRange = nmScan.all_hosts()
+		for host in hostRange:
+			print('Target: %s (%s)' % (host, nmScan[host].hostname()))
+			#return the information from the banner
+			scannedBanner = nmScan[host].all_protocols()
+			for proto in scannedBanner:
+				meh = nmScan[host][proto].keys()
+				print('Protocol: %s Software: %s Version: %s' % (nmScan[host][proto][80]['name'],nmScan[host][proto][80]['product'],nmScan[host][proto][80]['version']))
+				print('Banner %s' % (nmScan[host][proto][80]['script']))
+
 
 #input the test ip address and port range using user input
 userIP = input("Please enter your IP range:\n")
 userPort = input("Please enter your port range:\n")
-testing = Portscan(userIP,userPort)
+#testing = Portscan(userIP,userPort)
+testBanner = Bannergrab(userIP,userPort)
 #run the port scan
-testing.results()
+#testing.results()
+testBanner.results()
