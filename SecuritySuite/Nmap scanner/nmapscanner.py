fc7ff597e16149e8a40ec61777e7171cf4fb5681
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-08-08 16:37:04 +0100

    Add files via upload
    Updated portscan class for cleaner results.  Now iterated through for loops for information.

--- SecuritySuite/Nmap scanner/nmapscanner.py
+++ SecuritySuite/Nmap scanner/nmapscanner.py
@@ -4,19 +4,33 @@ import nmap
 #set up the nmap command for port scanning
 nmScan = nmap.PortScanner()
 
-#parent class that instantiates the ip address and ports
+#parent class that contains the instantiation module
 class Scanning:
 	def __init__(self,ipaddress,ports):
 		self.ipaddress = ipaddress
 		self.ports = ports
 
-#Child class of Scanning.  Runs the port scan and returns results.
+#child class for open port scanning
 class Portscan(Scanning):
 	def results(self):
-		test = nmScan.scan(self.ipaddress)
-		print(test)
+		#scan the ip and port range
+		nmScan.scan(self.ipaddress, self.ports)
+		#put all scanned hosts to a variable
+		hostRange = nmScan.all_hosts()
+		#run for loop for hosts in range
+		for host in hostRange:
+			print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+			#run for loop for all protocols returned
+			for proto in nmScan[host].all_protocols():
+				print('Protocol : %s' % proto)
+				#put returned ports into keys
+				scannedPorts = nmScan[host][proto].keys()
+				#run for loop for returned ports
+				for port in scannedPorts:
+					print('port: %s\tstate: %s' % (port, nmScan[host][proto][port]['state']))
 
-#input the test ip address and port range.
-p = Portscan('127.0.0.1','21-443')
-#run the port scan.
-p.results()
+
+#input the test ip address and port range
+testing = Portscan('127.0.0.1','21-443')
+#run the port scan
+testing.results()
