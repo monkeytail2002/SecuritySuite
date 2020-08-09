ce8d8294bf1ed2e55109e5269c3ac3c896400cc8
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-08-09 19:31:59 +0100

    Add files via upload

--- SecuritySuite/Nmap scanner/Switchtest.py
+++ SecuritySuite/Nmap scanner/Switchtest.py
@@ -6,7 +6,17 @@ import portscan
 
 userIP = "127.0.0.1"
 userPort = "80"
-userScan = input("Choose scan type\n")
+userScan = input("Choose scan type\n"
+                 "1. Default TCP scan\n"
+                 "2. TCP SYN scan - Use this for stealthy scans.\n"
+                 "3. UDP scan.\n"
+                 "4. SCTP scan - Use this for Telecom networks.\n"
+                 "5. Null scan - Use this for RST scanning.  Not suitable for Windows.\n"
+                 "6. FIN scan - Use this for RST scanning.  Not suitable for Windows.\n"
+                 "7. Xmas scan - Use this for RST scanning.  Not suitable for Windows.\n"
+                 "8. TCP ACK scan.  Use this to determine firewall rules.\n"
+                 "9. Advanced SCTP scan.\n"
+                 "10. IP Protocol scan.\n" )
 
 
 
@@ -20,4 +30,28 @@ if userScan == 1:
     pscan.results()
 elif userScan == 2:
     pscan = portscan.Stealthscan(userIP, userPort)
+    pscan.results()
+elif userScan == 3:
+    pscan = portscan.UDPScan(userIP, userPort)
+    pscan.results()
+elif userScan == 4:
+    pscan = portscan.Sigtran(userIP, userPort)
+    pscan.results()
+elif userScan == 5:
+    pscan = portscan.Nullscan(userIP, userPort)
+    pscan.results()
+elif userScan == 6:
+    pscan = portscan.Finnscan(userIP, userPort)
+    pscan.results()
+elif userScan == 7:
+    pscan = portscan.Xmasscan(userIP, userPort)
+    pscan.results()
+elif userScan == 8:
+    pscan = portscan.TCPAckscan(userIP, userPort)
+    pscan.results()
+elif userScan == 9:
+    pscan = portscan.Cookiescan(userIP, userPort)
+    pscan.results()
+elif userScan == 10:
+    pscan = portscan.IPscan(userIP, userPort)
     pscan.results()
--- SecuritySuite/Nmap scanner/portscan.py
+++ SecuritySuite/Nmap scanner/portscan.py
@@ -14,7 +14,13 @@ class Portscan(Scanning):
     def results(self):
         scanner.scan(self.ipaddress,self.ports)
         hostRange = scanner.all_hosts()
-        print(hostRange)
+        for host in hostRange:
+            print('Host: %s (%s)' %(host,scanner[host].hostname()))
+            for proto in scanner[host].all_protocols():
+                print('Protocol: %s' % proto)
+                scannedPorts = scanner[host][proto].keys()
+                for port in scannedPorts:
+                    print('Port: %s\tState: %s' % (port,scanner[host][proto][port]['state']))
 
 
 class Stealthscan(Scanning):
@@ -28,3 +34,45 @@ class Stealthscan(Scanning):
                 scannedPorts = scanner[host][proto].keys()
                 for port in scannedPorts:
                     print('Port: %s\tState: %s' % (port,scanner[host][proto][port]['state']))
+
+class UDPScan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sU')
+        print(test)
+
+class Sigtran(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sY')
+        print(test)
+
+
+class Nullscan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sN')
+        print(test)
+
+
+class Finnscan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sF')
+        print(test)
+
+class Xmasscan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sX')
+        print(test)
+
+class TCPAckscan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sA')
+        print(test)
+
+class Cookiescan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sZ')
+        print(test)
+
+class IPscan(Scanning):
+    def results(self):
+        test = scanner.scan(self.ipaddress,self.ports,arguments='-sO')
+        print(test)
