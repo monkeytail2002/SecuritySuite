4d4c374bc78005390ef5181664963c149538a3c3
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2021-01-06 19:36:09 +0000

    Update bannergrab.py

--- SecuritySuite/Nmap scanner/bannergrab.py
+++ SecuritySuite/Nmap scanner/bannergrab.py
@@ -1,29 +1,20 @@
-#!/usr/bin/env python
-
-#import required modules
-import nmap
-#Set the scanner for the nmap module
-scanner = nmap.PortScanner()
-
-#parent class that contains the instantiation module
-class Scanning:
-	def __init__(self,ipaddress,ports):
-		self.ipaddress = ipaddress
-		self.ports = ports
-
-
-#Class for grabbing banners (meta information about networks and servers for opsec
 class Bannergrab(Scanning):
 	def results(self):
 		#scan the ip range, port range and insert the banner script along with the -sV option
 		scanner.scan(self.ipaddress, self.ports,arguments='-sV -script=banner')
 		#set the scan to return the host
 		hostRange = scanner.all_hosts()
-		for host in hostRange:
+        for host in hostRange:
 			print('Target: %s (%s)' % (host, scanner[host].hostname()))
-			#return the information from the banner
-			scannedBanner = scanner[host].all_protocols()
-			for proto in scannedBanner:
-				meh = scanner[host][proto].keys()
-				print('Protocol: %s Software: %s Version: %s' % (scanner[host][proto][80]['name'],scanner[host][proto][80]['product'],scanner[host][proto][80]['version']))
-				print('Banner %s' % (scanner[host][proto][80]['script']))
+            #return the information from the banner by port
+            for proto in scanner[host].all_protocols():
+                print('Protocol: %s' % proto)
+                scannedBanner = scanner[host][proto].keys()
+                for port in scannedBanner:
+                    print('Port: %s' % (port))
+                    print('State|Reason: %s|%s' % (scanner[host][proto][port]['state'],scanner[host][proto][port]['reason']))
+                    print('Name: %s' % (scanner[host][proto][port]['name']))
+                    print('Product|Version: %|%s' % (scanner[host][proto][port]['product'],scanner[host][proto][port]['version']))
+                    print('Extra Information: %s' % (scanner[host][proto][port]['extrainfo']))
+                    print('CPE: %s' % (scanner[host][proto][port]['cpe']))
+                    print('Banner %s' % (scanner[host][proto][port]['script']))
